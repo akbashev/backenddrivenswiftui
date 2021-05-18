70fece59446b3f8953ff8eae51d56cd9a9bcfbb5
Jaleel Akbashev <adzhalil@gmail.com>
2021-05-18 11:59:07 +0200

    Added image

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -26,7 +26,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.sizeThatFits)
     }
 }
 
